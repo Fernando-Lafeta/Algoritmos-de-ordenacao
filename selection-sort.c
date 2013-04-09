e0d759e55673d072eb984207428dbcfa8f50e211
Fernando Lucas <fernando.lafeta@gmail.com>
2013-04-09 10:20:54 -0300

    Codigo do selecao alterado

--- selection-sort.c
+++ selection-sort.c
@@ -1,25 +1,32 @@
 #include <stdio.h>
-#define max 10
 
-void selectionsort(int *vet, int n) {
-	int i, j, x;
-	for (i = 0; i < n; i++) {
-		for (j = i+1; j < n; j++) {
-			if (vet[i] > vet[j]) {
-				x = vet[i];
-				vet[i] = vet[j];
-				vet[j] = x;
-			}
-		}
-	}
+void Selecao(int vetor[], int tamanho){
+    int i,j,indice_menor,tmp;
+
+    for (i = 0; i <tamanho-1; i++) {
+    indice_menor = i;
+    for (j = (i+1); j < tamanho; j++) {
+      if(vetor[j] < vetor[indice_menor]) {
+        indice_menor = j;
+      }
+    }
+    if (i != indice_menor) {
+       tmp = vetor[i];
+      vetor[i] = vetor[indice_menor];
+      vetor[indice_menor] = tmp;
+    }
+  }
 }
-
-int main() {
-	int vetor[max] = {5,2,7,8,10,6,1,4,9,3};
-	int i;
-	selectionsort(vetor,max);
-	for (i = 0; i < max; i++) {
-		printf("%d ", vetor[i]);
-	}
-	return(0);
+int main(){
+    int vetor[10],i;
+    for(i=0; i<10 ; i++){
+             scanf("%d",&vetor[i]);
+             }
+    Selecao(vetor,10);
+    printf("\n\n\n");
+    for(i=0; i<10 ; i++){
+             printf("\n %d",vetor[i]);
+    }
+    system("pause");
+    return(0);
 }
